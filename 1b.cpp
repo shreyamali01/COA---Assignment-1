087638341da31f19b1cbf62c35793e30adc21464
shreyamali01 <shreya.mali@iitgn.ac.in>
2024-08-14 21:39:52 +0530

    code for fibonacci using for loop

---
+++ 1b.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long foo(int n){
+
+    if (n<=1){
+        return n;
+    }
+
+    long long prev1 = 1;
+    long long prev2 = 0;
+    long long ans;
+
+    for(int i=2; i<=n; i++){
+        ans = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = ans;
+    }
+    return ans;
+}
+
+int main(){
+    int n = 50;
+    for (int i=0; i<n; i++){
+        cout << i << " : " << foo(i) << endl;
+    }
+    
+}
