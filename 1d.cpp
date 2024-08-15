f1275688e53320f057c0fdb7634746f5c42e8640
shreyamali01 <shreya.mali@iitgn.ac.in>
2024-08-15 13:51:48 +0530

    adding fibonacci using for loop and memoization

---
+++ 1d.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long foo(int n, vector<long long> dp){
+    
+    if (n==0){
+        return 0;
+    }
+
+    if (n==1){
+        return 1;
+    }
+
+    if (dp[n]!=-1){
+        return dp[n];
+    }
+
+    long long prev2 = 0;
+    long long prev1 = 1;
+    long long ans;
+
+    for (int i=2; i<=n; i++){
+        ans = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = ans;
+    }
+    return dp[n]=ans;
+
+    
+}
+
+int main(){
+    int n = 50;
+    vector <long long> dp(n+1,-1);
+    for (int i=0; i<n; i++){
+        cout << i << " : " << foo(i,dp);
+    }
+    return 0;
+}
